77b2dc1d244319954c76f6f39428d883ab3c626e
labib0x0hunter <labibfaisal450@gmail.com>
2026-02-25 22:58:46 +0600

    test

--- main.go
+++ main.go
@@ -5,27 +5,30 @@ import (
 	"net/http"
 )
 
-var templ = template.Must(template.ParseFiles("templates/hello.html"))
+// var templ = template.Must(template.ParseFiles("templates/hello.html"))
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query().Get("name")
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+// 	q := r.URL.Query().Get("name")
 
-	data := template.HTML(q)
+// 	data := template.HTML(q)
 
-	templ.Execute(w, map[string]any{
-		"Name": data,
-	})
-}
+// 	templ.Execute(w, map[string]any{
+// 		"Name": data,
+// 	})
+// }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("http://ip:port/echo?name=Labib"))
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("http://ip:port/echo?name=Labib"))
+// }
 
 func main() {
 
-	http.HandleFunc("/echo", helloHandler)
-	http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/echo", helloHandler)
+	// http.HandleFunc("/", homeHandler)
 
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("")
 
 }
